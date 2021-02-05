78e894e58db063d2e29ab6a1714c80412b4328d8
Maria Shkurko <shkurko_mashenka@mail.ru>
2021-02-05 11:15:29 +0300

    Lesson 6 is completed

---
+++ js/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const isNumber = (num) => {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+const randomInteger = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const gameBot = () => {
+
+    let number = randomInteger(1, 100);
+
+    const isWin = (number) => {
+
+        let userNumber = prompt('Угадай число от 1 до 100');
+
+        if (userNumber === null) {
+            alert('Игра окончена');
+        } else if (!isNumber(userNumber)) {
+            alert('Введи число!');
+            isWin(number);
+        } else if (parseInt(userNumber) === number) {
+            alert('Поздравляю, Вы угадали!!!');
+        } else if (parseInt(userNumber) > number) {
+            alert('Загаданное число меньше');
+            isWin(number);
+        } else {
+            alert('Загаданное число больше');
+            isWin(number);
+        }
+    };
+
+    isWin(number);
+};
+
+gameBot();
